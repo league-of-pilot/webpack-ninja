8f16b8f0fe624348060d4bdfc71979efb1046c73
Danh Tran <danh2k@nexondv.com>
2024-07-22 14:57:34 +0700

    1st try - error lost all css

--- C3-plugin/webpack.config.js
+++ C3-plugin/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -9,4 +10,18 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+      chunks: ["index"],
+      inject: true,
+      filename: "index.html",
+    }),
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/products.html"),
+      chunks: ["product"],
+      inject: true,
+      filename: "products.html",
+    }),
+  ],
 };
